31b184ed960103605877b883814452e8c8254df8
vtjaeger <viniciustjaeger@gmail.com>
2024-01-10 16:01:12 -0300

    new method to find by status (active t or f)

--- src/main/java/com/example/springboot/repositories/BrandRepository.java
+++ src/main/java/com/example/springboot/repositories/BrandRepository.java
@@ -1,12 +1,16 @@
 package com.example.springboot.repositories;
 
 import com.example.springboot.models.BrandModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface BrandRepository extends JpaRepository<BrandModel, UUID> {
     BrandModel findByName(String name);
+    Page<BrandModel> findByActiveTrue(Pageable pageable);
 }
